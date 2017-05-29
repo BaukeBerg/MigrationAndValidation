a31b631e9cb84233d4174cb5bedc3c2b57f7927d
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-05-29 16:56:54 +0200

    added rascal jar in local dir

--- Migration/src/executeConsole.bat
+++ Migration/src/executeConsole.bat
@@ -1,4 +1,4 @@
 @echo off
 rem execute rascal_unstable.jar, run the console.rsc module
-"C:\Program Files\Java\jdk1.8.0_121\bin\java.exe" -jar ".\..\..\..\rascal_unstable.jar" console.rsc
+"C:\Program Files\Java\jdk1.8.0_121\bin\java.exe" -jar rascal_unstable.jar console.rsc
 pause
